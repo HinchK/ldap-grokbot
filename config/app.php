55490d4e507031864102844fa2ffc410b0eb4b43
hinchk <kasey.hinchman@gmail.com>
2019-02-19 03:04:50 -0800

    added debugbar, tighted up.gitignore

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Barryvdh\Debugbar\Facade;
+
 return [
 
     /*
@@ -13,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Grokbot'),
 
     /*
     |--------------------------------------------------------------------------
